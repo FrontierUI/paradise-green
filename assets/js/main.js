927087e2b7f15546916b844479786a534bb8d951
FrontierUI <uifrontier95@gmail.com>
2024-10-23 17:02:53 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+$('.paradiseProdSlider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: { slidesToShow: 3, slidesToScroll: 1, infinite: true },
+    },
+
+    { breakpoint: 600, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+    { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+  ],
+});
+
+const items = document.querySelectorAll('.accordion button');
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+$('.smPostSlider').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: { slidesToShow: 1, slidesToScroll: 1 },
+    },
+    {
+      breakpoint: 480,
+      settings: { slidesToShow: 1, slidesToScroll: 1 },
+    },
+  ],
+});
